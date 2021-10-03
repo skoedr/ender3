56adce1b80d7f1052207747820f64370fca42c79
Robin <robin.wald@hotmail.de>
2021-10-03 19:55:27 +0200

    bed size adaption for petsfang back tp 235x235

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1346,8 +1346,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 230
-#define Y_BED_SIZE 230
+#define X_BED_SIZE 235
+#define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
